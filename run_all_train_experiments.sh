f6b31811d1083c72f5b2fc758f0f52ac9e841712
Jervis <jervismuindi@yahoo.co.uk>
2019-03-09 20:28:52 -0800

    Add training experiments with all weights unfrozen for the pretrained models

--- run_all_train_experiments.sh
+++ run_all_train_experiments.sh
@@ -31,3 +31,28 @@ echo "Resnet Done"
 echo ">>> Training Alexnet Raw..."
 python cnn.py --max_iter 100 --model_name=alexnet --data_folder=data/processed_casia2_224 --cuda=$CUDA --l2_regularization=$REG --experiment_name "l2reg=${REG}_iter=100"
 echo "Alexnet Done"
+
+####
+
+# Repeat with full weights unlocked.
+NUM_ITER=3
+
+# Inception
+echo ">>> Training Inception all weights..."
+python cnn.py --max_iter $NUM_ITER --model_name=inception_pretrained --data_folder=data/processed_casia2_299  --cuda=$CUDA --l2_regularization=$REG --unfreeze_all_weights=True --experiment_name "l2reg=${REG}_iter=${NUM_ITER}_trainallweights"
+echo "Inception Done"
+
+# DenseNet
+echo ">>> Training Densenet all weights..."
+python cnn.py --max_iter $NUM_ITER --model_name=densenet_pretrained --data_folder=data/processed_casia2_224 --cuda=$CUDA --l2_regularization=$REG --unfreeze_all_weights=True --experiment_name "l2reg=${REG}_iter=${NUM_ITER}_trainallweights"
+echo "DenseNet Done"
+
+# Resnet
+echo ">>> Training Resnet all weights..."
+python cnn.py --max_iter $NUM_ITER --model_name=resnet_pretrained --data_folder=data/processed_casia2_224 --cuda=$CUDA --l2_regularization=$REG --unfreeze_all_weights=True --experiment_name "l2reg=${REG}_iter=${NUM_ITER}_trainallweights"
+echo "Resnet Done"
+
+echo ">>> Training VGG16 all weights ..."
+python cnn.py --max_iter $NUM_ITER --model_name=vgg16_pretrained --data_folder=data/processed_casia2_224  --cuda=$CUDA --l2_regularization=$REG --unfreeze_all_weights=True --experiment_name "l2reg=${REG}_iter=${NUM_ITER}_trainallweights"
+echo "VGG Done"
+
