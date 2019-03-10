3bb5c9b947ca0b93ec2241bfc4ef3a9ec2f7d4ca
Jervis <jervismuindi@yahoo.co.uk>
2019-03-09 20:10:07 -0800

    Add script to run a batch of training experiments

--- run_all_train_experiments.sh
+++ run_all_train_experiments.sh
@@ -1,1 +1,33 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
+set -x
+
+NUM_ITER=10
+REG=0
+CUDA=True
+
+
+# VGG
+echo ">>> Training VGG16 ..."
+python cnn.py --max_iter $NUM_ITER --model_name=vgg16_pretrained --data_folder=data/processed_casia2_224  --cuda=$CUDA --l2_regularization=$REG --experiment_name "l2reg=${REG}_iter=${NUM_ITER}"
+echo "VGG Done"
+
+# Inception
+echo ">>> Training Inception..."
+python cnn.py --max_iter $NUM_ITER --model_name=inception_pretrained --data_folder=data/processed_casia2_299  --cuda=$CUDA --l2_regularization=$REG --experiment_name "l2reg=${REG}_iter=${NUM_ITER}"
+echo "Inception Done"
+
+# DenseNet
+echo ">>> Training Densenet..."
+python cnn.py --max_iter $NUM_ITER --model_name=densenet_pretrained --data_folder=data/processed_casia2_224 --cuda=$CUDA --l2_regularization=$REG --experiment_name "l2reg=${REG}_iter=${NUM_ITER}"
+echo "DenseNet Done"
+
+# Resnet
+echo ">>> Training Resnet..."
+python cnn.py --max_iter $NUM_ITER --model_name=resnet_pretrained --data_folder=data/processed_casia2_224 --cuda=$CUDA --l2_regularization=$REG --experiment_name "l2reg=${REG}_iter=${NUM_ITER}"
+echo "Resnet Done"
+
+# Alexnet Raw
+echo ">>> Training Alexnet Raw..."
+python cnn.py --max_iter 100 --model_name=alexnet --data_folder=data/processed_casia2_224 --cuda=$CUDA --l2_regularization=$REG --experiment_name "l2reg=${REG}_iter=100"
+echo "Alexnet Done"
