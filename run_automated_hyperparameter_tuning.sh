4413a33b42ba9b10501dc88d6d9238cb6b0fb6f4
Jervis <jervismuindi@yahoo.co.uk>
2019-03-17 11:33:27 -0700

    do 3 hrs worht of tuning

--- run_automated_hyperparameter_tuning.sh
+++ run_automated_hyperparameter_tuning.sh
@@ -6,10 +6,10 @@ echo "Starting Hyperparameter tuning ..."
 CUDA=True
 
 # Want to spend 2 hours doing random explorations @ 30 minutes per trial.
-INIT_POINTS=4
+INIT_POINTS=2
 # Resnet model 1 epoch takes ~2 minutes. So for 15 epochs max, that's 30 minutes runtime.
 # For an overnight run, we can explore 24 samples.
-MAX_ITER=24
+MAX_ITER=6
 DATA_FOLDER="data/processed_casia2_224"
 
 MODEL_NAME="resnet_pretrained"
