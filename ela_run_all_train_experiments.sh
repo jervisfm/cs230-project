50514a5e095fd53c4d346969ef0497a854c102fe
Raj Prateek Kosaraju <kosaraju@fb.com>
2019-03-17 12:12:15 -0700

    Change order of experiments

--- ela_run_all_train_experiments.sh
+++ ela_run_all_train_experiments.sh
@@ -57,6 +57,14 @@ CUDA=True
 
 
 
+REG=0.001
+# Run Resnet with different params
+echo ">>> Training Resnet all weights..."
+python cnn.py --max_iter $NUM_ITER --model_name=resnet_pretrained --data_folder=data/processed_casia2_224_ela --cuda=$CUDA --l2_regularization=$REG --unfreeze_all_weights=True --batch_size=50 --learning_rate=0.00025 --experiment_name "l2reg=${REG}_iter=${NUM_ITER}_batch_size=50_learning_rate=0.00025_trainallweights_ela"
+echo "Resnet Done"
+
+REG=0
+
 # Re-run experiments which OOMd, with smaller batch size. 
 #Inception
 echo ">>> Training Inception all weights..."
@@ -69,9 +77,3 @@ echo ">>> Training Densenet all weights..."
 python cnn.py --max_iter $NUM_ITER --model_name=densenet_pretrained --data_folder=data/processed_casia2_224_ela --cuda=$CUDA --l2_regularization=$REG --unfreeze_all_weights=True --batch_size=50 --experiment_name "l2reg=${REG}_iter=${NUM_ITER}_batch_size=50_trainallweights_ela"
 echo "DenseNet Done"
 
-
-REG=0.001
-# Run Resnet with different params
-echo ">>> Training Resnet all weights..."
-python cnn.py --max_iter $NUM_ITER --model_name=resnet_pretrained --data_folder=data/processed_casia2_224_ela --cuda=$CUDA --l2_regularization=$REG --unfreeze_all_weights=True --batch_size=50 --learning_rate=0.00025 --experiment_name "l2reg=${REG}_iter=${NUM_ITER}_batch_size=50_learning_rate=0.00025_trainallweights_ela"
-echo "Resnet Done"
